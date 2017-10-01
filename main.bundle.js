f11c5faae1855ea1c89bf23dcd4dcc9dc483a867
lizhuo24 <lizhuo24@wanda.cn>
2017-10-01 10:18:51 +0800

    Commit message

--- main.bundle.js
+++ main.bundle.js
@@ -58,6 +58,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'app';
+        this.f = 1;
     }
     return AppComponent;
 }());
